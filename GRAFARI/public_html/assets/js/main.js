c0fa19db9a8803db318775ccf1ddbd97424b85af
janus <mcdemon2001@yahoo.de>
2014-11-11 14:01:03 +0100

    stand zur angebotspräse

--- GRAFARI/public_html/assets/js/main.js
+++ GRAFARI/public_html/assets/js/main.js
@@ -6,6 +6,27 @@
 $(function(){
 	console.log('Setting up ...');
 	miSearch_init();
+
+	var $container = $('#results');
+
+	$('.subQuery').click(function(){
+		var id = $(this).attr("data-id");
+
+		$container.isotope({
+		  itemSelector: '.result',
+	  	  layoutMode: 'fitRows',
+		  filter: id
+		});
+	});
+
+	$('.mainQuery').click(function(){
+		$container.isotope({
+		  itemSelector: '.result',
+	  	  layoutMode: 'fitRows',
+	  	  filter: ''
+		});
+	});
+
 });
 
 /**
@@ -14,26 +35,52 @@ $(function(){
 function miSearch_init(){
 	// Setup Button Handler
 	miSearch_reg_btn();
+
+
+	// Example text for the demo
+    $(".form-control").val("All people who live in Germany AND ( people who are self-employed OR NOT people who are homeless )");
 }
 
 /**
  * Register Page-Handler
  */
 function miSearch_reg_btn(){
-	var searchForm = $('#searchForm');
+	var brandRow = $('#brandRow');
 	var resultWell = $('#resultWell');
+	var queryHistory = $('#queryHistory');
+	var resultSpinner = $('#resultSpinner');
+	var results = $('#results');
 	
 	$('#btn_search').click(function(){
-		searchForm.removeClass('center');
+		brandRow.removeClass('center');
 		
 		setTimeout(function(){
 			resultWell.removeClass('hidden');
+			queryHistory.removeClass('hidden');
 		}, 400);
+		
+		setTimeout(function(){
+			resultSpinner.addClass('hidden');
+			results.removeClass('hidden');
+			init_isotope();
+		}, 1000);
 	});
 	
 	$('#btn_clear').click(function(){
 		resultWell.addClass('hidden');
-		searchForm.addClass('center');
+		brandRow.addClass('center');
+		resultSpinner.removeClass('hidden');
+		results.addClass('hidden');
 	});
 	
+}
+
+function init_isotope(){
+	var $container = $('#results');
+	// init
+	$container.isotope({
+	  // options
+	  itemSelector: '.result',
+	  layoutMode: 'fitRows'
+	});
 }
