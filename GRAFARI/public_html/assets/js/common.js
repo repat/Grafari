827f132fa6021165b2825b1efe13f5e1a265ba53
janus <mcdemon2001@yahoo.de>
2014-11-11 17:03:22 +0100

    RequireJS integration Readded

---
+++ GRAFARI/public_html/assets/js/common.js
@@ -0,0 +1,20 @@
+//The build will inline common dependencies into this file.
+//For any third party dependencies, like jQuery, place them in the lib folder.
+//Configure loading modules from the lib directory,
+//except for 'app' ones, which are in a sibling
+//directory.
+requirejs.config({
+	baseUrl: 'assets/js/lib',
+	paths: {
+		app: '../app',
+		bootstrap: 'bootstrap',
+		isotope: 'isotope',
+		jquery: 'jquery',
+		jqueryBridget: 'jquery.bridget'
+	},
+	shim: {
+		isotope: {
+			deps: ['jquery', 'jqueryBridget']
+		}
+	}
+});
