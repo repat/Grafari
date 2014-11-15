388febd71a6a98ddaf2e3ec56fd6cbea443bd790
lSoleyl <lSoleyl+github@gmail.com>
2014-11-15 12:18:00 +0100

    Anfrageparsing für's Backend geschrieben.
     * Parst natürlichsprachliche Sätze und wandelt sie in Facebook-URLs um
     * .gitignore für node_modules/ und temporäre Dateien erstellt
     * Testscript 'test-parser.js' erstellt
     * TODO ID-Resovler schreiben und (evt. parser für deutsche Sätze schreiben)

---
+++ backend/nodejs/request-logic/parser-main.js
@@ -0,0 +1,71 @@
+//  ######  Language: ######
+// 
+//CombinedRequest = BasicRequest { 'OR' BasicRequest }
+//BasicRequest    = Selector {('AND'|'who') Condition}
+//Selector        = ['all'] ('people'|'men'|'women')
+//Condition       = ['who'] (CondYounger|CondOlder|...|CondGroup)
+//CondYounger     = 'are' ('younger' 'than' | 'under') Age
+//CondOlder       = 'are' ('older' 'than' | 'over') Age 
+//CondAgeBetween  = 'are' 'between' Age 'and' Age
+//CondAgeEqual    = 'are' Age
+//CondLiveIn      = ('live')|('are' 'living') 'in' token
+//CondLike        = 'like' token
+//CondName        = 'are' ('named'|'called') token
+//CondGroup       = '(' Disjunction ')'
+//Disjunction     = Conjunction {'OR' Conjunction}
+//Conjunction     = Condition {'AND' Condition }
+
+//Age             = integer ['years'] ['old']
+
+
+var L = require("./parser-logic-en")
+var R = require("./group-resolver")
+var T = require("./tree-translator")
+
+
+// exported functions
+exports.tokenize      = L.tokenize
+exports.tokenizeEn    = L.tokenize
+exports.tokenizeDe    = undefined
+
+exports.parse         = flatParse
+exports.rawParse      = rawParse
+exports.flatParse     = flatParse
+exports.translateTree = T.translate
+exports.translate     = T.translate
+exports.resolveGroups = R.resolveGroups
+
+
+/** Parses the given sentence according to the defined grammar and then resolves nested requests by  
+ *  factoring out inner groups (eg. A AND (B OR C) --> A AND B OR A AND C). This conversion must be done, before
+ *  passing the tree to translate(). Otherwise translate will fail.
+ *
+ * @param sentence the sentence to parse
+ *
+ * @return the flattened parse tree which can be passed to translate() to convert it into Facebook request URLs
+ */
+function flatParse(sentence) {
+  var tree = rawParse(sentence)
+  return R.resolveGroups(tree)
+}
+
+/** Just tokenizes and then parses the given sentence according to the defined grammar.
+ *
+ * @param sentence the sentence which should be parsed
+ *
+ * @return the parse tree of the sentence. Might throw exceptions if sentence format doesn't match the grammar
+ */
+function rawParse(sentence) {
+  var tokens = L.tokenize(sentence)
+  tokens.reverse()
+  var tree = L.RootNode(tokens)
+
+  if (tokens.length > 0) 
+    throw "Parse error: parsing request returned with non empty token list: " + tokens.reverse()
+
+  return tree;
+}
+
+
+
+
