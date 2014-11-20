127aca76fad5d265be9316244bfb673e23d8f10b
lSoleyl <lSoleyl+github@gmail.com>
2014-11-20 19:07:10 +0100

    ID-Resolver in Anfrageparsing eingebaut
     * lib/graph.js erstmal auf App-Token umgestellt, da die (hoffentlich) nicht auslaufen
     * lib/graph.js kann jetzt entweder nach Namen oder nach Orten IDs auflösen. (wird vom Resolver benötigt)
     * lib/graph.js wird jetzt als Resolver des Anfrage-Parsers genutzt ('node testparser.js' löst jetzt Germany zu einer ID auf)

--- backend/nodejs/lib/graph.js
+++ backend/nodejs/lib/graph.js
@@ -1,32 +1,95 @@
 var graph = require('fbgraph');
 
-var getIdFromLocation = function(location, callback) {
+// Exported functions
+exports.getIdFromName     = getIdFromName
+exports.getIdFromLocation = getIdFromLocation
 
-    // das ist der Usertoken von hier und läuft nach einer Stunde ab: https://developers.facebook.com/tools/accesstoken/
-    graph.setAccessToken('CAACEdEose0cBADcdv7qHUNC832Jn0a0oUnAfd6raljnPeZCZCbWL1SLcJnTO5w25DCE5Uv8GsejjauZC2k9SBP4Noh1wdKuvMXBAKcxGCOtdsR1FZCoyZCj21H8j2D36fDMwxCJZA2tCjFooSd1VNxxwJcYIrKeAxVS8MUWoA5QLlfLy08EzjvN1k3xh2rPlBMTkd9eYTAowZDZD');
-
-    // extending static access token
-    graph.extendAccessToken({
-        "client_id":      "736322176438280"
-      , "client_secret":  "b15e0263baa65d34312aaf3a0ad8bc44"
-    }, function (err, facebookRes) {
-       //console.log(facebookRes);
-    });
-
-    var searchOptions = {
-        q:    location,
-        type: 'page'
-    };
-
-    graph.search(searchOptions, function(err, res) {
-      if (err) 
-        return callback(err)
+// das ist der Usertoken/Apptoken von hier und läuft nach einer Stunde ab: https://developers.facebook.com/tools/accesstoken/
+graph.setAccessToken('736322176438280|N8bcT0U2C4-PHlvoJpqe8ytN1Y8'); //<- AppToken (sollte nicht auslaufen)
 
 
-      // FB gibt manchmal nichts zurück. Einfach nochmal probieren.
-      callback(null, res.data[0].id);
-    });
+/** A generic method to query the facebook graph for a certain ID
+ */
+function lookupData(name, callback) { 
+  var searchOptions = {
+      q:    name,
+      type: 'page'
+  };
 
+  graph.search(searchOptions, function(err, res) {
+    if (err) 
+      return callback(err)
+
+    if (res.data.length > 0)
+      return callback(null, res);
+    return callback("No matching ID found for: '" + name + "'!")
+  });
+}
+
+
+/** Resolves a name to the first ID it finds
+ */
+function getIdFromName(name, callback) {
+  lookupData(name, function(err, res) {
+    if (err)
+      return callback(err)
+
+    //if everything went well, return first ID
+    return callback(null, res.data[0].id) 
+  })  
 };
 
-exports.getIdFromLocation = getIdFromLocation;
+
+/** Helper function for implementing getIdFromLocation()
+ */
+ function array_findFirst(ary, predicate) {
+  for(var c = 0; c < ary.length; ++c)
+    if(predicate(ary[c]))
+      return ary[c]
+  return undefined
+}
+
+function array_contains(ary, predicate) {
+  return array_findFirst(ary, predicate) != undefined
+}
+
+/** This resolves the given location name into an ID
+ *  It only accepts IDs of the category City, Country, State, Landmark
+ * 
+ */
+function getIdFromLocation(location, callback) {
+  lookupData(location, function(err, res) {
+    if (err)
+      return callback(err)
+
+    var locationObject = array_findFirst(res.data, function(element) {
+      if (element.category_list != undefined)
+        return array_contains(element.category_list, function(entry) {
+          return entry.name == "City" || 
+                 entry.name == "Country" ||
+                 entry.name == "State" ||  //eg. Florida
+                 entry.name == "Landmark"  //eg. Africa
+        })
+      return false
+    })
+
+    if (!locationObject)
+      return callback("Couldn't map location: '" + location + "' to an ID corresponding to a city, a country, a state or a landmark")
+    callback(null, locationObject.id)
+  })
+}
+
+
+
+
+
+
+/** Function to extend the duration of the access token.
+ *  At the moment this is not needed
+ */
+function extendAccessToken(callback) {
+  graph.extendAccessToken({
+    "client_id":      "736322176438280"
+  , "client_secret":  "b15e0263baa65d34312aaf3a0ad8bc44"
+  }, callback)
+}
--- backend/nodejs/lib/request-logic/resolvers.js
+++ backend/nodejs/lib/request-logic/resolvers.js
@@ -1,10 +1,39 @@
+/** This module defines the resolver which is used to map from names 
+ *  and locations to facebook IDs
+ */
+
+module.exports = GraphResolver
 
 
 
-//TODO when a concrete resolver has been implemented, replace this one
-module.exports = DummyResolver
+/** Resolver class using the lib/graph.js implementation to look up IDs
+ */
+function GraphResolver() {
+  this.graph = require("../graph")
+}
+
+/**Used only for 'who live in X'
+ * The ID returned must be of type city or country for Facebook to accept it
+ *
+ * @param location the location to search for
+ * @param callback the callback which gets the result (since resolve might need a network lookup)
+ */
+GraphResolver.prototype.resolveLocation = function(location, callback) {
+  this.graph.getIdFromLocation(location, callback)
+}
+
+/** Used to generally resolve Names to ID's  (eg. for 'who like X')
+ * 
+ * @param what the item to search for
+ * @param callback the callback which gets the result (since resolve might need a network lookup)
+ */ 
+GraphResolver.prototype.resolve = function(what, callback) {
+  this.graph.getIdFromName(what, callback)
+}
 
 
+
+/// ################### DummyResolver ###########################
 // A mock class which just returns ID(elem) instead of a correct ID
 function DummyResolver() {}
 
