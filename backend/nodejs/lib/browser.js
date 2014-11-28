ff7d30e80325e82dbf696c3b3c0399809850d64b
repat <repat@repat.de>
2014-11-28 18:55:30 +0100

    div extraction and starting with regex

--- backend/nodejs/lib/browser.js
+++ backend/nodejs/lib/browser.js
@@ -131,27 +131,62 @@ function convertPageToJSON(browser) {
 
   // first result (handeled differently, because its in a different div)
   var person = {};
-  person.name = browser.text('#u_0_p > div > div > div > div._zs.fwb > a');
-  person.profession = browser.text('#u_0_p > div > div > div > div._pac._dj_');
-  person.from = browser.text('#u_0_p > div > div > div > div._946 > div > div:nth-child(1) > div');
 
+  // name, pictureurl and id are set here
+  person.name = browser.text('#u_0_p > div > div > div > div._zs.fwb > a');
+  //person.pictureurl = "";
+  //TODO: how do i only get the href value?
+  person.id = extractUserIdFromURLInASuperHackyWayWithRegex(browser.html('#u_0_p > div > div > div > div._zs.fwb > a'));
+
+  //trying to find the right tag for the src 
+  console.log(browser.text('#u_0_p > div > div > img'));
+
+  var allTheDivs = [];
+
+  // subtitle
+  allTheDivs[allTheDivs.length] = browser.text('#u_0_p > div > div > div > div._pac._dj_');
+  // could be in a loop
+  allTheDivs[allTheDivs.length] = browser.text('#u_0_p > div > div > div > div._946 > div > div:nth-child(1) > div');
+  allTheDivs[allTheDivs.length] = browser.text('#u_0_p > div > div > div > div._946 > div > div:nth-child(2) > div');
+  allTheDivs[allTheDivs.length] = browser.text('#u_0_p > div > div > div > div._946 > div > div:nth-child(3) > div');
+  allTheDivs[allTheDivs.length] = browser.text('#u_0_p > div > div > div > div._946 > div > div:nth-child(4) > div');
+
+  // debug
+  // console.log(allTheDivs)
+
+  // add all the other attributes (not sure about syntax here)
+  // person = analyzeRegex(allTheDivs);
+  // person will be created out of the results above and then pushed into final result
   people.push(person);
 
 
+  // not changed to new parsing ideas (#22) yet
   // other results
   var rawHtml = browser.query('#u_0_o_browse_result_below_fold > div');
   for(var i=0; i<rawHtml._childNodes.length; i++) {
     var index = i+1;  // i+1 inside the browser.text string doesn't work
+
     var person = {};
+
+    // name, pictureurl and id are set here
     person.name = browser.text('#u_0_o_browse_result_below_fold ._4_yl:nth-of-type(' + index + ') div[data-bt*=title] > a'); 
+    person.pictureurl = "";
+    person.id = "";
 
-    person.profession = browser.text('#u_0_o_browse_result_below_fold ._4_yl:nth-of-type(' + index + ') div[data-bt*=sub_headers] > a');
+    allTheDivsFromOtherPeople = [];
 
-    if(browser.text('#u_0_o_browse_result_below_fold ._4_yl:nth-of-type(' + index + ') div[data-bt*=snippets] ._52eh:nth-child(1)').substring(0,9) === 'Lives in ') {
-      person.from = browser.text('#u_0_o_browse_result_below_fold ._4_yl:nth-of-type(' + index + ') div[data-bt*=snippets] ._ajw:nth-of-type(1) ._52eh > a:nth-of-type(1)');
-     } //else {
-    //   console.log(i + ': has no city!');
-    // }
+    // loop possible here
+    allTheDivsFromOtherPeople[allTheDivsFromOtherPeople.length] = browser.text('#u_0_o_browse_result_below_fold ._4_yl:nth-of-type(' + index + ') div[data-bt*=sub_headers] > a');
+    allTheDivsFromOtherPeople[allTheDivsFromOtherPeople.length] = browser.text('#u_0_o_browse_result_below_fold ._4_yl:nth-of-type(' + index + ') div[data-bt*=snippets] ._ajw:nth-of-type(1) ._52eh');
+    allTheDivsFromOtherPeople[allTheDivsFromOtherPeople.length] = browser.text('#u_0_o_browse_result_below_fold ._4_yl:nth-of-type(' + index + ') div[data-bt*=snippets] ._ajw:nth-of-type(2) ._52eh');
+    allTheDivsFromOtherPeople[allTheDivsFromOtherPeople.length] = browser.text('#u_0_o_browse_result_below_fold ._4_yl:nth-of-type(' + index + ') div[data-bt*=snippets] ._ajw:nth-of-type(3) ._52eh');
+    allTheDivsFromOtherPeople[allTheDivsFromOtherPeople.length] = browser.text('#u_0_o_browse_result_below_fold ._4_yl:nth-of-type(' + index + ') div[data-bt*=snippets] ._ajw:nth-of-type(4) ._52eh');
+
+    // debug
+    //console.log(allTheDivsFromOtherPeople)
+
+    // add all the other attributes (not sure about syntax here)
+    //person = analyzeRegex(allTheOtherDivs);
 
     people.push(person);
   }
@@ -161,4 +196,35 @@ function convertPageToJSON(browser) {
 
 
   return people;
+}
+
+function analyzeRegex(divs) {
+  // check for empty strings first
+  var person = {};
+    // forEach nach ecma5+?
+ /*   for(var i = 0; i < divs.length; i++) {
+      subStrings = splitStrings(divs[i]);
+      // analye them in sub-for-loop
+        // person.attribute = subStrings[j];   
+      }
+   */   
+  return person;
+}
+
+function splitStrings(divLine) {
+  returnArray = [];
+  /*
+  if (divLine.indexOf("·") != -1) {
+    tmp = divLine.subString[0,divLine.indexOf("·")];
+  }*/
+
+  return returnArray;
+}
+
+function extractUserIdFromURLInASuperHackyWayWithRegex(url) {
+  //please forgive me for my uselessness with regex
+  var pattern = /<a href="https:\/\/www.facebook.com\//
+  var pattern2 = /[?][\w|\W|\d|\D]*/
+  url2 = url.replace(pattern, "")
+  return url2.replace(pattern2, "")
 }
