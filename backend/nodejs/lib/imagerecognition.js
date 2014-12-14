9bfadf3c804b16ae21142e10380d91a7738bf5d6
Malte Heidenreich <malteh@users.noreply.github.com>
2014-12-14 17:58:59 +0100

    imagerecognition + redis

--- backend/nodejs/lib/imagerecognition.js
+++ backend/nodejs/lib/imagerecognition.js
@@ -1,7 +1,9 @@
 var request = require("request")
+var redis = require("redis")
+var rc = redis.createClient()
 
 // Exported functions
-exports.imageToTags = imageToTags
+exports.imageToTags = imageToTagsCache
 
 function imageToTags(url, callback) {
   var url = "http://api.imagga.com/draft/tags?api_key=acc_0cc34ea494b2b58&url=" + encodeURIComponent(url)
@@ -24,10 +26,35 @@ function imageToTags(url, callback) {
   })
 }
 
-tagsToList = function(tags, confidence) {
-  var tags = tags.filter(function(tag) {
-    return tag.confidence >= confidence;
-  });
+function imageToTagsCache(url, callback) {
+  rc.hget("url", url, function(err,reply) {
+    if (!reply) {
+      return imageToTags(url, function(e, r) {
+        rc.hset("url", url, JSON.stringify(r))
+        return callback(e, r)
+      })
+    } else {
+      return callback(null, JSON.parse(reply))
+    }
+  })
+  return
+  rc.hget("url", url, function(err,reply) {
+    if (err || !reply) {
+      console.log(err.message)
+      return imageToTags(url, function(e, r) {
+        rc.hset("url", url, r)
+        return callback(e, r)
+      })
+    }
+    console.log(reply)
+    return callback(null, reply)
+  })
+}
 
-  return tags.map(function(elem) { return elem.tag; });
-};
+tagsToList = function(tags, confidence) {
+    var tags = tags.filter(function(tag) {
+      return tag.confidence >= confidence;
+    });
+
+    return tags.map(function(elem) { return elem.tag; });
+  };
